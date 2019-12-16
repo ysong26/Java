da968f781ad7161ed3d51a859ea0b82f845947cd
ysong26 <q2235133@naver.com>
2019-12-16 11:18:21 +0900

    java

---
+++ Day-20190704_first/src/p31_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/*
+ * 작성일 : 2019-07-04
+ * 작성자 : 서예송
+ * 요  약 : 자바에서 명령어 방식으로 입력 처리하는 방법
+ */
+
+public class p31_1 {
+
+	public static void main(String[] args) {
+		//자바에서 명령어 방식으로 입력 처리하는 방법
+		
+		//1.args를 이용하는 방법 준비X
+/*		String str1=args[0];
+		String str2=args[1];
+		
+		System.out.println("Str1 = "+str1);
+		System.out.println("Str2 = "+str2);*/
+	
+		//2.Scanner를 이용하는 방법 준비O
+/*		Scanner in = new Scanner(System.in);
+		String str=in.nextLine();
+		
+		System.out.println("Scanner str = " + str);*/
+		
+	}
+
+}
